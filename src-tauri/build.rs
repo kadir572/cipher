883d89064d3ddfd74f226fdf635e81a0c693e589
kadir572 <kadir9625@gmail.com>
2024-11-23 22:01:20 +0100

    pass env var from build.rs into binary

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,5 +5,9 @@ fn main() {
         Ok(val) => println!("cargo:warning=STRIPE_SECRET_KEY is available: {}", val),
         Err(e) => eprintln!("cargo:warning=Error accessing STRIPE_SECRET_KEY: {}", e),
     }
+
+    if let Ok(stripe_secret) = std::env::var("STRIPE_SECRET_KEY") {
+        println!("cargo:rustc-env=STRIPE_SECRET_KEY={}", stripe_secret);
+    }
     tauri_build::build()
 }
