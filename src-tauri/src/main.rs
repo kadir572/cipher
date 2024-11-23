124cec322d7e543edd32db7c7694854a663f7a2b
kadir572 <kadir9625@gmail.com>
2024-11-23 05:33:43 +0100

    add logs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
 fn main() {
+    match env::var("STRIPE_SECRET_KEY") {
+        Ok(val) => println!("Stripe Secret Key is available!"),
+        Err(e) => eprintln!("Error accessing STRIPE_SECRET_KEY: {}", e),
+    };
     app_lib::run();
 }
