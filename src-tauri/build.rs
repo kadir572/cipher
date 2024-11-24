a672d570877d935bd3ac067e74506cb75513458e
kadir572 <kadir9625@gmail.com>
2024-11-24 09:35:30 +0100

    fix yml

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,3 @@
-use std::env;
-
 fn main() {
-    // match env::var("STRIPE_SECRET_KEY") {
-    //     Ok(val) => println!("cargo:warning=STRIPE_SECRET_KEY is available: {}", val),
-    //     Err(e) => eprintln!("cargo:warning=Error accessing STRIPE_SECRET_KEY: {}", e),
-    // }
     tauri_build::build()
 }
